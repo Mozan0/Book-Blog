3cb95f9781c4ea1b1d84a63b4e70312db393982f
ozan <mehmet.gol@std.antalya.edu.tr>
2023-07-26 17:10:15 +0300

    Fixed image display

--- src/app/add-book-dialog/add-book-dialog.component.ts
+++ src/app/add-book-dialog/add-book-dialog.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Inject, Output } from '@angular/core';
 import * as Bucket from '@spica-devkit/bucket';
-import * as Storage from '@spica-devkit/storage';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -8,10 +7,10 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   templateUrl: './add-book-dialog.component.html',
   styleUrls: ['./add-book-dialog.component.css']
 })
-
 export class AddBookDialogComponent {
   selectedImage: File | null = null;
   imagePreview: string | ArrayBuffer | null = null;
+
   constructor(
     public dialogRef: MatDialogRef<AddBookDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
@@ -25,12 +24,18 @@ export class AddBookDialogComponent {
     const files: FileList | null = inputElement?.files;
     if (files && files.length > 0) {
       const selectedFile = files[0];
-      this.photoSelected.emit(selectedFile);
+      this.selectedImage = selectedFile;
+
+      // Read the file content and convert it to a data URL
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.imagePreview = e.target?.result as string | ArrayBuffer;
+      };
+      reader.readAsDataURL(selectedFile);
     }
   }
-  
+
   async addBooks() {
-    
     try {
       const newDocument = {
         title: (<HTMLInputElement>document.getElementById('title')).value,
@@ -38,15 +43,14 @@ export class AddBookDialogComponent {
         id: (<HTMLInputElement>document.getElementById('id')).value,
         price: Number((<HTMLInputElement>document.getElementById('price')).value),
         stock: Number((<HTMLInputElement>document.getElementById('stock')).value),
-        image: (<HTMLInputElement>document.getElementById('image')).value, 
+        image: this.imagePreview ? this.imagePreview.toString() : '', // Use the image data URL if available
       };
-    await Bucket.data.insert("64b91d543404ce002c98bf90", newDocument);
-    } 
-    catch (err) {
-      console.log("Error adding product:", err);
+
+      await Bucket.data.insert('64b91d543404ce002c98bf90', newDocument);
+      // await Storage.insert(newDocument.image);
+    } catch (err) {
+      console.log('Error adding product:', err);
     }
     this.dialogRef.close();
   }
-
-
 }
